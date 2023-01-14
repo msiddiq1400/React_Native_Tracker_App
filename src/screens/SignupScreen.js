28f63e640991c6263be9274bde2f045625c657d9
siddiq <msiddiq1400@gmail.com>
2023-01-14 21:52:40 +0500

    navigation added with screens

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,24 @@
-import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { Input, Text, Button } from '@rneui/themed';
+import { StyleSheet } from 'react-native';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({navigation}) => {
   return (
     <>
-      <Text>Signup Screen</Text>
-      <Button title='Go to Signin' onPress={() => navigation.navigate('Signin')}/>
-      <Button title='Go to Main Flow' onPress={() => navigation.navigate('BottomTabNavigator')}/>
+        <Spacer><Text h3>Sign Up for Tracker</Text></Spacer>
+        <Input label="Email" />
+        <Input label="Password" />
+        <Spacer><Button title="Sign Up"/></Spacer>
+
+      {/* <Button title='Go to Signin' onPress={() => navigation.navigate('Signin')}/>
+      <Button title='Go to Main Flow' onPress={() => navigation.navigate('BottomTabNavigator')}/> */}
     </>
   )
 }
+
+const styles = StyleSheet.create({
+
+})
 
 export default SignupScreen
